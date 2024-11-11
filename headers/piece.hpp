bf458190d7e241a2bb086b1a7fd93b2dcd728dcc
anakin004 <160974989+anakin004@users.noreply.github.com>
2024-11-10 23:51:28 -0500

    Update piece.hpp

--- headers/piece.hpp
+++ headers/piece.hpp
@@ -1,17 +1,20 @@
 #include <vector>
 
+// similar to game.hpp
+// making everything static made this project really simple, not best practice - i know
+
 class Piece{
 
     public:
-        void static spawnTetromino();
-        void static moveLeft();
-        void static moveRight();
-        void static moveDown();
-        void static lockPiece( std::vector<std::pair<int, int>>& );
+        static void spawnTetromino();
+        static void moveLeft();
+        static void  moveRight();
+        static void  moveDown();
+        static void  lockPiece( std::vector<std::pair<int, int>>& );
         static std::vector<std::pair<int, int>> getRelativePos( std::vector<std::pair<int, int>>& p);
         static std::pair<int, int> subFromCenter( std::pair<int,int> pos, std::pair<int,int> twoPos);
-        void static Rotate( bool direction );
-        bool static AbleRotate( std::vector<std::pair<int,int>> modGlobalPos );
+        static void Rotate( bool direction );
+        static bool AbleRotate( std::vector<std::pair<int,int>> modGlobalPos );
 
         static int tetrominoType;
 
